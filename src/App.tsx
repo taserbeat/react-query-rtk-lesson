29114c96b25a40c9bd7727864b4033503efa33df
taserbeat <taserbeat@gmail.com>
2021-09-10 23:42:24 +0900

    従来手法のfetchで動作を確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { VFC } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { StateProvider } from './contexts/StateProvider';
+import ClassicalFetchA from './components/ClassicalFetchA';
+import ClassicalFetchB from './components/ClassicalFetchB';
 
 import Layout from './components/Layout';
 
 const App: VFC = () => {
   return (
     <BrowserRouter>
-      <Layout>App</Layout>
+      <StateProvider>
+        <Layout>
+          <Switch>
+            <Route exact path="/fetch-a">
+              <ClassicalFetchA />
+            </Route>
+
+            <Route exact path="/fetch-b">
+              <ClassicalFetchB />
+            </Route>
+          </Switch>
+        </Layout>
+      </StateProvider>
     </BrowserRouter>
   );
 };
